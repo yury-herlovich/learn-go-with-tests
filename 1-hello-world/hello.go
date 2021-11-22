ad496425b64448c2736681de5c332f9567548583
Yury Herlovich <yury@bstock.com>
2021-11-22 15:51:26 -0800

    add parameters to 1-hello

--- 1-hello-world/hello.go
+++ 1-hello-world/hello.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + ", " + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Yury", ""))
 }
