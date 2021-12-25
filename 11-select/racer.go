7c2c47d6e98bcead43f205cfe7aa93a6d10c8e15
Yury Herlovich <yuryherlovich@gmail.com>
2021-12-25 16:24:55 -0500

    11 select

---
+++ 11-select/racer.go
@@ -0,0 +1,28 @@
+package racer
+
+import (
+	"errors"
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", errors.New("timeout")
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
+}
---
+++ 11-select/racer_test.go
@@ -0,0 +1,55 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("return fastest server", func(t *testing.T) {
+		slowServer := makeTestServer(20 * time.Millisecond)
+		fastServer := makeTestServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+
+		want := fastUrl
+		got, _ := Racer(slowUrl, fastUrl, 50*time.Millisecond)
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("return an error after 10 seconds", func(t *testing.T) {
+		server := makeTestServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := Racer(server.URL, server.URL, 10*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error")
+		}
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("expected %q but got %q", want, got)
+	}
+}
+
+func makeTestServer(delay time.Duration) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	return server
+}
